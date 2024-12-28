a97fc7999d74015115bc94a77943b629e97d053e
DonYall <highvoltage9886@gmail.com>
2024-12-27 23:52:51 -0500

    change website content for SEO

--- src/About.tsx
+++ src/About.tsx
@@ -26,14 +26,14 @@ function About() {
       <div className="col-span-2 lg:col-span-1 p-10 lg:p-40 lg:pr-16 my-auto">
         <h2 className="text-4xl font-normal font-montserrat leading-none">ABOUT</h2>
         <p className="text-md mt-8 text-gray-300">
-          I am a 2nd year aerospace engineering co-op student at Carleton University. I have learnt and developed many technical skills and am eager to apply my
-          knowledge to real-world engineering problems. I am Always looking for opportunities to grow and learn in the aerospace industry and am committed to
-          making a significant impact in the field of Aerospace Engineering.
+          I am a second-year aerospace engineering co-op student at Carleton University with a strong foundation in technical skills and a passion for applying
+          my knowledge to real-world engineering challenges. I am constantly seeking opportunities to grow and learn within the aerospace industry, driven by a
+          commitment to making a meaningful impact in the field of aerospace engineering.
         </p>
         <p className="text-md mt-8 text-gray-300">
-          My passion for aerospace stems from the ability to engineer “seemingly” impossible technologies. From the Apollo missions to reusable boosters, goals
-          that were seemingly impossible were accomplished successfully through complex engineering. Similarly, I hope to be at the forefront of engineering
-          innovations, enabling the solutions for future “impossible” feats.
+          My passion for aerospace comes from the ability to engineer technologies that once seemed impossible. From the Apollo missions to reusable rockets,
+          challenges once deemed insurmountable were overcome through advanced engineering. Likewise, I aspire to be at the forefront of groundbreaking
+          innovations, contributing to solutions that achieve the "impossible" in the future.
         </p>
       </div>
       <div className="col-span-2 lg:col-span-1 grid grid-cols-2 gap-8 p-10 lg:p-20">
